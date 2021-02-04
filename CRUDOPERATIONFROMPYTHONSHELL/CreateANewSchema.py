c25f1a7ba6188c6a984de1e6c80dbfe0dd2e6788
Onabanjo Titus <74909457+OnabanjoTitus@users.noreply.github.com>
2021-02-04 12:36:53 +0100

    creating a new schema

---
+++ CRUDOPERATIONFROMPYTHONSHELL/CreateANewSchema.py
@@ -0,0 +1,32 @@
+import mysql.connector
+
+from mysql.connector import Error
+
+
+def create_dataBase():
+    conn = None
+
+    try:
+        conn = myDB = mysql.connector.connect(
+            host="localhost",
+            user="user1",
+            password="Welcome@1")
+        myCursor = conn.cursor()
+        myCursor.execute("CREATE DATABASE myDatabaseFromShell")
+
+        if conn.is_connected():
+            print("Database Is created")
+    except Error as e:
+        print("Cannot be created due to ", e)
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+            print('database shutdown!!!!')
+
+
+def main():
+    create_dataBase()
+
+
+if __name__ == '__main__':
+    main()
